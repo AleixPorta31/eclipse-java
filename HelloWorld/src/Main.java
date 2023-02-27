6c4695e6b8f67b86c4b49164ddaddd546ab92941
AleixPorta31 <aleixportaeij@gmail.com>
2023-02-27 14:26:30 +0100

    ejercicios JAVA

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -3,7 +3,58 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("!Hola mundo!");
+//		System.out.println("!Hola mundo!");
+//		//System.out.printf("5 + 6 + 7 = %d", 5 + 6 + 7).println();
+//		
+//		int a = 2, b = 5, c = 3;
+//		
+//		System.out.printf("%d + %d + %d = %d", a, b, c, a+b+c).println();
+//		
+//		c = 6;
+//		
+//		System.out.printf("%d + %d + %d = %d", a, b, c, a+b+c).println();	
+//		
+//		int suma = a+b+c;
+//		
+//		System.out.println(a +"+"+ b +"+"+ c + "=" + suma);
+
+		int a = 9, b = 3, c = 2, d = 5;
+
+		if ((a + b) > (c + d)) {
+			System.out.println("a + b es mayor que c + d");
+		} else {
+			System.out.println("a + b es igual o menor que c + ");
+		}
+
+//		if((a+b) <= (c+d)) {
+//		System.out.println("a + b es igual o menor que b + c");
+//		}
+
+		int angulo1 = 180, angulo2 = 0, angulo3 = 0;
+
+		if ((angulo1 + angulo2 + angulo3) == 180) {
+			System.out.println("es un triangulo");
+		} else {
+			System.out.println("no es un triangulo");
+		}
+
+		int z = 31;
+		if (z % 2 == 0) {
+			System.out.println("es par");
+		} else {
+			System.out.println("es impar");
+		}
+
+		int num1 = 1, num2 = 9, num3 = 3;
+		if (num1 > num2 && num1 > num3) {
+			System.out.println("num 1 es el mayor");
+		} else if (num2 > num1 && num2 > num3) {
+			System.out.println("num 2 es el mayor");
+		} else if (num3 > num1 && num3 > num2) {
+			System.out.println("num 3 es el mayor");
+		} else {
+			System.out.println("hay almenos 2 numeros iguales");
+		}
 
 	}
 
---
+++ HelloWorld/src/Scaner.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class Scaner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		Scanner keyboard = new Scanner(System.in);
+//		System.out.printf("Escribe un numero:").println();
+//		int numero = keyboard.nextInt();
+//		keyboard.close();
+//		
+//		
+//		if(numero == 1) {
+//			
+//			System.out.println("has escrito un 1");
+//		}else if(numero == 2) {
+//			System.out.println("has escrito un 2");
+//		}else if(numero == 3) {
+//			System.out.println("has escrito un 3");
+//		}else if(numero == 4) {
+//			System.out.println("has escrito un 4");
+//		}else if(numero == 5) {
+//			System.out.println("has escrito un 5");
+//		}else if(numero == 6) {
+//			System.out.println("has escrito un 6");
+//		}else {
+//			System.out.println();
+//		}
+//		
+//		
+		Scanner keyboard = new Scanner(System.in);
+
+		System.out.println("Escribe un numero:");
+
+		int numero = keyboard.nextInt();
+		keyboard.close();
+		
+		int i = 2;
+
+		while(numero % i != 0 && i < numero-1) {
+			i++;			
+		}
+		
+		if (i == numero-1 || numero == 2) {
+			System.out.println("El numero es primo");
+		}else if (numero <= 1) {
+			System.out.println("Has puesto un 0, un 1 o un numero negativo");
+		}else {
+			System.out.println("El numero no es primo");
+		}
+		
+//		else if (numero == 2){
+//			System.out.println("El numero es primo");
+//		}else if (numero <= 1) {
+//			System.out.println("Has puesto un 0, un 1 o un numero negativo");
+//		}
+
+	}
+
+}
