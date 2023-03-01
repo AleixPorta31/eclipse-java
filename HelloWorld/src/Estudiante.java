bb50143b60ba9560f52ddec410aef9c0f8f3997f
AleixPorta31 <aleixportaeij@gmail.com>
2023-03-01 14:39:44 +0100

    funciones y metodos

--- HelloWorld/src/Estudiante.java
+++ HelloWorld/src/Estudiante.java
@@ -1,4 +1,41 @@
 
-public class Estudiante {
-
+public class Estudiante extends Persona{
+//	Constructores
+	public Estudiante(float sumaNotas, float notaMedia) {
+		this.numeroNotas = 5;
+		this.sumaNotas = sumaNotas;
+		this.notaMedia = notaMedia;
+	}
+	
+//	Atributos
+	private int numeroNotas;
+	private float sumaNotas;
+	private float notaMedia;
+	
+//	Metodos
+	public void agregarNota() {};
+	public void mostrarInfo() {};
+	public void crearEstudiante() {}
+	
+	
+//	Getters y setters
+	public int getNumeroNotas() {
+		return numeroNotas;
+	}
+	public void setNumeroNotas(int numeroNotas) {
+		this.numeroNotas = numeroNotas;
+	}
+	public float getSumaNotas() {
+		return sumaNotas;
+	}
+	public void setSumaNotas(float sumaNotas) {
+		this.sumaNotas = sumaNotas;
+	}
+	public float getNotaMedia() {
+		return notaMedia;
+	}
+	public void setNotaMedia(float notaMedia) {
+		this.notaMedia = notaMedia;
+	}
+	
 }
--- HelloWorld/src/Profesor.java
+++ HelloWorld/src/Profesor.java
@@ -1,4 +1,48 @@
 
-public class Profesor {
+public class Profesor extends Persona {
+
+//	Constructores
+	public Profesor(int edadEstudiantes, float sumaEdadEstudiantes, float edadMediaEstudiantes) {
+		super();
+		this.edadEstudiantes = edadEstudiantes;
+		this.sumaEdadEstudiantes = sumaEdadEstudiantes;
+		this.edadMediaEstudiantes = edadMediaEstudiantes;
+	}
+//	Atributos
+		private int edadEstudiantes;
+		private float sumaEdadEstudiantes;
+		private float edadMediaEstudiantes;
+
+//	Metodos
+	public void agregarEdadEstudiantes() {};
+	public void mostrarInfo() {}
+	
+//	getters setters
+
+	public int getEdadEstudiantes() {
+		return edadEstudiantes;
+	}
+
+	public void setEdadEstudiantes(int edadEstudiantes) {
+		this.edadEstudiantes = edadEstudiantes;
+	}
+
+	public float getSumaEdadEstudiantes() {
+		return sumaEdadEstudiantes;
+	}
+
+	public void setSumaEdadEstudiantes(float sumaEdadEstudiantes) {
+		this.sumaEdadEstudiantes = sumaEdadEstudiantes;
+	}
+
+	public float getEdadMediaEstudiantes() {
+		return edadMediaEstudiantes;
+	}
+
+	public void setEdadMediaEstudiantes(float edadMediaEstudiantes) {
+		this.edadMediaEstudiantes = edadMediaEstudiantes;
+	};
+	
+	
 
 }
