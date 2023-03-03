309341ce524fd392b06020cce2cfeac49cfe365e
AleixPorta31 <aleixportaeij@gmail.com>
2023-03-03 12:39:56 +0100

    ejerciciosStrings

--- HelloWorld/src/Strings/EjerciciosStrings.java
+++ HelloWorld/src/Strings/EjerciciosStrings.java
@@ -1,24 +1,116 @@
 package Strings;
+import java.util.Scanner;
 
 public class EjerciciosStrings {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-				
-		char letra;
-		
-		
-		
-		String palabra = "aleyo";
-		
-		System.out.println(palabra);
 
-		for(int i = 0; i < palabra.length(); i++) {
-			
-			letra = palabra.charAt(i);
-			System.out.println(letra);
-			
+//		char letra;		
+//		
+//		String palabra = "aleyo";
+//		
+//		System.out.println(palabra);
+//
+//		for(int i = 0; i < palabra.length(); i++) {
+//			
+//			letra = palabra.charAt(i);
+//			System.out.println(letra);
+//			
+//		}
+
+		
+//		//Ejercicio 2 Strings
+//		String pal1 = "aleyo";
+//		String pal2 = "aleix";
+//		
+//		int longitudPal1 = pal1.length();
+//		int longitudPal2 = pal2.length();
+//		
+//	
+//		int si = 0;
+//		int tambe = 1;
+//		
+//		String no = "No son iguales";
+//		String araSi = "Si son iguales";
+//		
+//		if (pal1.substring(0, 2).equals(pal2.substring(0, 2))) {
+//			si = 31;
+//		} else {
+//			si = 145679;
+//		}
+//		
+//		if (pal1.substring((longitudPal1 -2), longitudPal1).equals(pal2.substring((longitudPal2 -2), longitudPal2))) {
+//			tambe = 31;
+//		} else {
+//			si = 39476;
+//		}
+//		
+//		if(si == tambe) {
+//			System.out.println(araSi);
+//		}else {
+//			System.out.println(no);
+//		}
+		
+		
+//		//Ejercicio 3
+//		Scanner tuPalabra = new Scanner(System.in);
+//		System.out.printf("Escribe una palabra:").println();
+//		String palabra = tuPalabra.next();
+//		System.out.println("tu palabra es: " + palabra);
+//		tuPalabra.close();
+//				
+//		
+//		if (palabra.startsWith("abc") || (palabra.endsWith("abc"))) {
+//			System.out.println("La palabra contiene abc al princio y/o al final");
+//		} else if (palabra.contains("abc")) {
+//			System.out.println("la palabra " + palabra + " contiene abc");
+//		}else {
+//			System.out.println("La palabra " + palabra + " no contiene abc");
+//		}
+		
+		//Es CAPICUA?
+		Scanner tuPalabra = new Scanner(System.in);
+		System.out.printf("Escribe una palabra:").println();
+		String palabra = tuPalabra.next();		
+		tuPalabra.close();
+		
+		int numPar = 0;
+				
+		int mitad = palabra.length()/2;
+				
+		String principio = palabra.substring(0, mitad);
+		
+		for(int i = 0; i <= palabra.length(); i++) {
+			if (palabra.length() % 2 == 0) {
+				numPar = 1;
+			}else {
+				numPar = 2;
+			}				
 		}
+		
+		String finaal = "";
+		
+		if(numPar == 2) {
+			finaal = palabra.substring(mitad +1);
+		}else if(numPar == 1) {
+			finaal = palabra.substring(mitad);
+		}
+
+				
+		String principioRevertido = new StringBuilder(principio).reverse().toString();	
+				
+		if (principioRevertido.equals(finaal)) {
+			System.out.println("La palabra " + palabra + " SI es un palindromo");
+		}else {
+			System.out.println("La palabra " + palabra + " NO es un palindromo");
+		}
+		
+		
+		
+		
+		
+
 	}
 
 }
--- HelloWorld/src/Vehiculo/MainBici.java
+++ HelloWorld/src/Vehiculo/MainBici.java
@@ -1,14 +1,14 @@
-package Vehiculo;
-
-public class MainBici {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Bici RockyMountain = new Bici(2);
-		System.out.println(RockyMountain.getCambios());
-		
-		RockyMountain.setCambios(4);
-		System.out.println(RockyMountain.getCambios());
-	}
-
-}
+//package Vehiculo;
+//
+//public class MainBici {
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		Bici RockyMountain = new Bici(2);
+//		System.out.println(RockyMountain.getCambios());
+//		
+//		RockyMountain.setCambios(4);
+//		System.out.println(RockyMountain.getCambios());
+//	}
+//
+//}
